f0222d5de797545ed3fa67a9fdc744c21f353250
niteshkumaryadav16 <niteshkumar61725@gmail.com>
2024-04-23 17:57:36 +0530

    Comparission

---
+++ 04_comparision.js
@@ -0,0 +1,13 @@
+console.log("2" > 1);
+console.log("02" > 2);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0);// true
+
+
+console.log(undefined < 0);// false
+console.log(undefined == 0); // false
+
+// === strick check 
+console.log("2" === 2); 
