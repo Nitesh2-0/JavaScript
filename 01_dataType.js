bb9e1d94818f64d55370f6172f5cae77b8927ac8
niteshkumaryadav16 <niteshkumar61725@gmail.com>
2024-04-23 14:23:41 +0530

    data type

---
+++ 01_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(6+6)
+console.log('Hitesh');
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// data type -> primitive 
+/**
+ * number => 2 to power 53
+ * bigint
+ * String, 
+ * boolean => true/false
+ * null => standalone value -> type object
+ * undefined => abhi value assingned nahi nai -> undefined type
+ * symbol => unique
+ */
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
