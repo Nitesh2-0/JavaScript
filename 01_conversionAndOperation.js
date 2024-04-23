374d8c5be52e4299afe856f6f65b1cf967bef895
niteshkumaryadav16 <niteshkumar61725@gmail.com>
2024-04-23 14:49:44 +0530

    Opeartions

--- 01_conversionAndOperation.js
+++ 01_conversionAndOperation.js
@@ -35,3 +35,33 @@ let StringNumber = String(someNumber);
 
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+// *************** Operations ********************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2**3);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 1); // 121
+console.log(1 + 2 + "3"); // 33
+
+console.log(+true); // 1
+//console.log(true+);// it not allowed
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2+2; 
+
+let gameCounter = 100; 
+gameCounter++; 
+console.log(gameCounter); // 101
+++gameCounter; 
+console.log(gameCounter); // 102
