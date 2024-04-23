a97bc902d16de7f9e52bb512cb0f1601af826453
niteshkumaryadav16 <niteshkumar61725@gmail.com>
2024-04-23 13:48:44 +0530

    Day1

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144415;
+let accountEmail = "niteshkr@gmail.com"; 
+var password = "123456789";
+accountCity = "ray";
+
+accountCity = "Jaipur"; 
+// accountId = 2; // -> it not allowed
+
+accountEmail ="hc@gmail.com"
+password = '1236542'; 
+accountCity = "Jay Ram Pur"
+
+console.log(accountEmail)
+console.table([accountId,accountCity,password,accountEmail])
